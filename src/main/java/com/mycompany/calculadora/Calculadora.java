c3ca694acc51472ea578540ab8bed2536c3e5dc7
Davidal2098 <dvidorr165@g.educaand.es>
2024-03-04 10:15:13 +0100

    Clase calculadora base, con metodo de suma y de resta

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,48 @@
+package com.mycompany.calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadora {
+
+    public static Scanner escaner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Buenas, qué operación desea realizar:");
+        System.out.println("1-Suma");
+        System.out.println("2-Resta");
+        int eleccion = escaner.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                System.out.println("La suma es: " + suma());
+                break;
+            case 2:
+                System.out.println("La resta es: " + resta());
+                break;
+            default:
+                System.out.println("Operación no válida");
+                break;
+        }
+    }
+
+    public static int suma() {
+        System.out.println("Ingrese el primer número:");
+        int num1 = escaner.nextInt();
+        System.out.println("Ingrese el segundo número:");
+        int num2 = escaner.nextInt();
+        return num1 + num2;
+    }
+
+    public static int resta() {
+        System.out.println("Ingrese el primer número:");
+        int num1 = escaner.nextInt();
+        System.out.println("Ingrese el segundo número:");
+        int num2 = escaner.nextInt();
+        return num1 - num2;
+    }
+
+}
